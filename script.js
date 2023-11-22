6419aae974cec46efe0d7234f942ee146c034627
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-22 18:42:23 +0530

    added rendering

--- script.js
+++ script.js
@@ -10,9 +10,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const fetchPictures = async () => {
 	const response = await fetch(
-		`https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=12`
+		`https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=30`
 	)
 	const data = await response.json()
 
-	console.log(data)
+	appendPictures(data)
+}
+
+const appendPictures = (data) => {
+	const fragment = new DocumentFragment()
+
+	data.forEach((picture) => {
+		const imageContainer = document.createElement('div')
+		imageContainer.classList.add('image-container')
+
+		const anchor = document.createElement('a')
+		anchor.href = picture.links.html
+		anchor.target = '_blank'
+
+		const image = document.createElement('img')
+		image.classList.add('image')
+		image.src = picture.urls.regular
+		image.alt = picture.alt_description
+
+		anchor.appendChild(image)
+		imageContainer.appendChild(anchor)
+
+		fragment.appendChild(imageContainer)
+	})
+
+	picturesContainer.appendChild(fragment)
 }
